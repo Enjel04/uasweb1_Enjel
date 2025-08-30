12fda89da4e3350c829b872a541c91f47e6ab81a
Enjel04 <sambelmatah@aol.com>
2025-08-31 03:48:12 +0700

    Commit 9

---
+++ app/core/Constants.php
@@ -0,0 +1,5 @@
+<?php
+
+define('BASE_URL', 'http://localhost/uasweb1_enjel/public');
+define('CSS_URL', BASE_URL . '/css');
+define('JS_URL', BASE_URL . '/js');
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Halaman <?= $data['title']; ?></title>
+    <link rel="stylesheet" href="<?= CSS_URL; ?>/bootstrap.min.css">
 </head>
 <body>
