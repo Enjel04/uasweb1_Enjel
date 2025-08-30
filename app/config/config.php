f3b4307b16881c6dfa3058bc9704cd04c95a052e
Enjel04 <sambelmatah@aol.com>
2025-08-30 10:38:40 +0700

    Commit 3

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+// app/config/config.php
+return [
+    'host' => 'localhost',
+    'dbname' => 'uasweb1_db',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4'
+];
